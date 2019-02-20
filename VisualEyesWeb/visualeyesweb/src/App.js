4734c9187b265d2a34952537806d752fda7167b1
jacqueschen1 <jacqueschen1@gmail.com>
2019-02-19 17:01:08 -0800

    added firebase connectivity and emotions graph

--- VisualEyesWeb/visualeyesweb/src/App.js
+++ VisualEyesWeb/visualeyesweb/src/App.js
@@ -1,26 +1,25 @@
+/* eslint react/prop-types: 0 */
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo-ve.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
+        <head>
+        </head>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <p>
+            Welcome to the VisualEyes client website.
+        </p>
+        
+
       </div>
+      
     );
   }
 }
--- VisualEyesWeb/visualeyesweb/src/index.js
+++ VisualEyesWeb/visualeyesweb/src/index.js
@@ -1,3 +1,4 @@
+/* eslint react/prop-types: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
